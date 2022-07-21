e0aad9d2d22a8347cf173d50e408199fc511058a
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-21 11:51:08 +0530

    login, signup, dashboard component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InputComponent } from './shared/input/input.component';
 import { FoodItemComponent } from './shared/food-item/food-item.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    FoodItemComponent
+    FoodItemComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
