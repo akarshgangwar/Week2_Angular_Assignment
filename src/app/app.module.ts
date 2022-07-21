074fc2cacb557df491b8d50aab6b693036568289
Akarsh Gangwar <akarsh.gangwar@stackpro.in>
2022-07-21 11:46:00 +0530

    shared components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './shared/input/input.component';
+import { FoodItemComponent } from './shared/food-item/food-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    FoodItemComponent
   ],
   imports: [
     BrowserModule
